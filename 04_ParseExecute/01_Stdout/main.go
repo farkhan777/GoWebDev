36cbc252174eb7d299eb282769d0a77fe01be00e
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-02-17 22:56:34 +0700

    Initial commit

---
+++ 04_ParseExecute/01_Stdout/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+func main() {
+	tpl, err := template.ParseFiles("tpl.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// Do not use the above code in production
