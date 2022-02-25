60ad02ad51adbe4986b526070de4ed7c191ce02b
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-02-25 18:59:56 +0700

    Composition And Methods

---
+++ 11_CompositionAndMethods/01/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+type person struct {
+	Name string
+	Age  int
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	p1 := person{
+		Name: "Farkhan Hamzah Firdaus",
+		Age:  21,
+	}
+
+	err := tpl.Execute(os.Stdout, p1)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
