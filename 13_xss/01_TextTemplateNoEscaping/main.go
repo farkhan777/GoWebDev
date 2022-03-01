dfda7df6f199e6cc1e8123e686bfbc4774a29a63
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-03-01 22:55:36 +0700

    XSS Text Template No Escaping

---
+++ 13_xss/01_TextTemplateNoEscaping/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+type Page struct {
+	Title   string
+	Heading string
+	Input   string
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	home := Page{
+		Title:   "Nothing Escaped",
+		Heading: "Nothing is escaped with text/template",
+		Input:   `<script>alert("Yow!");</script>`,
+	}
+
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", home)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
