391c3c0329194e8249bc33899b4b34be2ae04b83
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-03-11 13:09:33 +0700

    Understanding TCP Servers Reader

---
+++ 15_UnderstandingTCPServers/04_Read-Write-SetDeadline/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":7777")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err)
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	err := conn.SetDeadline(time.Now().Add(10 * time.Second))
+	if err != nil {
+		log.Fatalln("Connection Timeout")
+	}
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(conn, "I heard you say: %s\n", ln)
+	}
+	defer conn.Close()
+
+	// now we get here
+	// the connection will time out
+	// that breaks us out of the scanner loop
+	fmt.Println("***CODE GOT HERE***")
+}
