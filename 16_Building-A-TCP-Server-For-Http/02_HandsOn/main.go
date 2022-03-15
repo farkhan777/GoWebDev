1629c58d6db7363a494a1443a811350db1ec1509
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-03-15 14:48:23 +0700

    Building a TCP Server For Http

--- 16_Building-A-TCP-Server-For-Http/02_HandsOn/main.go
+++ 16_Building-A-TCP-Server-For-Http/02_HandsOn/main.go
@@ -1,5 +1,69 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
 
+func main() {
+	li, err := net.Listen("tcp", ":7777")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	request(conn)
+
+	response(conn)
+}
+
+func request(conn net.Conn) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			//	request line
+			m := strings.Fields(ln)[0] // method
+			u := strings.Fields(ln)[1] // uri
+
+			fmt.Println("***METHOD", m)
+			fmt.Println("Farkhan Hamzah Firdaus")
+			fmt.Println("**URI", u)
+		}
+		if ln == "" {
+			// headers are done
+			break
+		}
+		i++
+	}
+}
+
+func response(conn net.Conn) {
+
+	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>Hello World</strong></body></html>`
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprintf(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprintf(conn, body)
 }
