ea30d21d277191b317b4ade891deaa236024cbe7
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-03-22 22:35:46 +0700

    Net Http Package Request

---
+++ 17_Understanding-Net-Http-Package/03_Request/02_Method/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/url"
+	"text/template"
+)
+
+type hotdog int
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data := struct {
+		Method      string
+		Submissions url.Values
+	}{
+		req.Method,
+		req.Form,
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":7777", d)
+}
