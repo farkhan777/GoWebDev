7c03e425a8530a8c43e661bc806f72d695a00a47
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-04-25 21:22:42 +0700

    Hands on

---
+++ 22_HandsOn/05/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":7777")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		scanner := bufio.NewScanner(conn)
+		for scanner.Scan() {
+			ln := scanner.Text()
+			fmt.Println(ln)
+			if ln == "" {
+				// When ln is empty, header is done
+				break
+			}
+		}
+
+		fmt.Println("Code got here.")
+		io.WriteString(conn, "I see you connected.")
+
+		conn.Close()
+	}
+}
