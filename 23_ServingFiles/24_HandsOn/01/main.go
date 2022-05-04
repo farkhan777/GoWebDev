a4c1aac5cf758f737bf8c75c68701a878c0c2201
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-05-04 22:35:50 +0700

    Serving files

---
+++ 23_ServingFiles/24_HandsOn/01/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/dog", dog)
+	http.HandleFunc("/dog.jpg", dogPic)
+	http.ListenAndServe(":7777", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, "foo ran")
+}
+
+func dog(w http.ResponseWriter, req *http.Request) {
+	tpl, err := template.ParseFiles("dog.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	tpl.ExecuteTemplate(w, "dog.gohtml", nil)
+}
+
+func dogPic(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "toby.jpg")
+}
