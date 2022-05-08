1235d17767a033f2d30683f78aab5ee84a31f66b
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-05-08 21:07:36 +0700

    Serving files

---
+++ 27_Passing-Data/03_Form-Get/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":7777", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	v := req.FormValue("q")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+		<form method="GET">
+			<input type="text" name="q">
+	 		<input type="submit">
+		</form>
+		<br>
+	`+v)
+}
