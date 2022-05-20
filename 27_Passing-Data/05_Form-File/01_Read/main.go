a7420fca9ff78b78b1db053405f2fb37b926de6b
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-05-21 01:51:26 +0700

    Serving files

---
+++ 27_Passing-Data/05_Form-File/01_Read/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":7777", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+
+	var s string
+	fmt.Println(r.Method)
+	if r.Method == http.MethodPost {
+
+		// open
+		f, h, err := r.FormFile("q")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+
+		// for your information
+		fmt.Println("\nfile:", f, "\nheader:", h, "\nerr", err)
+
+		// read
+		bs, err := ioutil.ReadAll(f)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		s = string(bs)
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+	<form method="POST" enctype="multipart/form-data">
+	<input type="file" name="q">
+	<input type="submit">
+	</form>
+	<br>`+s)
+}
---
+++ 27_Passing-Data/05_Form-File/02_Store/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":7777", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	var s string
+	if r.Method == http.MethodPost {
+		f, h, err := r.FormFile("q")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+
+		// for your information
+		fmt.Println("\nfile: ", f, "\nheader: ", h, "\nerr", err)
+
+		// read
+		bs, err := ioutil.ReadAll(f)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		s = string(bs)
+
+		// store on server
+		dst, err := os.Create(filepath.Join("./user/", h.Filename))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer dst.Close()
+
+		_, err = dst.Write(bs)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tpl.ExecuteTemplate(w, "index.gohtml", s)
+}
