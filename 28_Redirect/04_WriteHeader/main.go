06bb97078467533696bc4096029eb543e8bef061
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-05-22 01:14:49 +0700

    Serving files

---
+++ 28_Redirect/04_WriteHeader/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/barred", barred)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":7777", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("Your request method at foo: ", r.Method, "\n\n")
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Your request method at bar:", r.Method)
+	// process from data
+	w.Header().Set("Location", "/")
+	w.WriteHeader(http.StatusSeeOther)
+}
+
+func barred(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Your request method at barred:", r.Method)
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
