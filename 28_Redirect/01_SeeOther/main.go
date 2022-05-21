9810fe55edd57db3db2fdea584d604de12c9fac7
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-05-21 22:39:04 +0700

    Serving files

---
+++ 28_Redirect/01_SeeOther/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/barred", barred)
+	http.Handle("/favicon.icon", http.NotFoundHandler())
+	http.ListenAndServe(":7777", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("Your request method at foo: ", r.Method, "\n\n")
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Your request method at bar:", r.Method)
+	// process form submission here
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func barred(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Your request method at barred:", r.Method)
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
