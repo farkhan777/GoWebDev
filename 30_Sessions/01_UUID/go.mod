1b807f49908db2707e6eb5222d3826249d7ff5eb
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-05-24 23:59:17 +0700

    Cookies

---
+++ 30_Sessions/01_UUID/go.mod
@@ -0,0 +1,8 @@
+module 01_UUID
+
+go 1.16
+
+require (
+	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
---
+++ 30_Sessions/01_UUID/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	uuid "github.com/satori/go.uuid"
+	"net/http"
+)
+
+// For this code to run, you will need this package:
+// go get github.com/satori/go.uuid
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":7777", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("session")
+	if err != nil {
+		id, _ := uuid.NewV4()
+		cookie = &http.Cookie{
+			Name:  "session",
+			Value: id.String(),
+			// Secure: true,
+			HttpOnly: true,
+			Path:     "/",
+		}
+		http.SetCookie(w, cookie)
+	}
+	fmt.Println(cookie)
+}
