7fdd096052674e16580a360c8ee49b51c8f91630
farkhan <farhanhamzah71@webmail.umm.ac.id>
2022-06-01 17:24:00 +0700

    Sessions

--- 30_Sessions/06_Logout/main.go
+++ 30_Sessions/06_Logout/main.go
@@ -15,13 +15,11 @@ type user struct {
 }
 
 var tpl *template.Template
-var dbUsers = map[string]user{}
+var dbUser = map[string]user{}
 var dbSession = map[string]string{}
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
-	bs, _ := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.MinCost)
-	dbUsers["test@gmail.com"] = user{Username: "test@gmail.com", Password: bs, First: "Farkhan", Last: "Hamzah"}
 }
 
 func main() {
@@ -29,6 +27,7 @@ func main() {
 	http.HandleFunc("/bar", bar)
 	http.HandleFunc("/signup", signup)
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":7777", nil)
 }
@@ -40,7 +39,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func bar(w http.ResponseWriter, r *http.Request) {
 	u := getUser(w, r)
-	if !alreadyLoggedIn(r) {
+	if !alreadyLoggedIn(w, r) {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
@@ -48,32 +47,27 @@ func bar(w http.ResponseWriter, r *http.Request) {
 }
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	if alreadyLoggedIn(r) {
+	if alreadyLoggedIn(w, r) {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
 	var u user
 
-	// process form submission
 	if r.Method == http.MethodPost {
 		un := r.FormValue("username")
 		p := r.FormValue("password")
 		f := r.FormValue("firstname")
 		l := r.FormValue("lastname")
 
-		if _, ok := dbUsers[un]; ok {
-			http.Error(w, "Username already taken", http.StatusForbidden)
+		// username taken ?
+		_, ok := dbUser[un]
+		if ok {
+			http.Error(w, "username already taken", http.StatusForbidden)
 			return
 		}
 
-		//_, ok := dbUsers[un]
-		//if ok {
-		//	http.Error(w, "Username already taken", http.StatusForbidden)
-		//	return
-		//}
-
-		// create session
+		// create dbSession
 		sID, _ := uuid.NewV4()
 		c := &http.Cookie{
 			Name:  "session",
@@ -82,24 +76,24 @@ func signup(w http.ResponseWriter, r *http.Request) {
 		http.SetCookie(w, c)
 		dbSession[c.Value] = un
 
-		// store user in dbUsers
 		bs, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.MinCost)
 		if err != nil {
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 			return
 		}
-		u = user{un, bs, f, l}
-		dbUsers[un] = u
+		u = user{Username: un, Password: bs, First: f, Last: l}
+		dbUser[un] = u
 
 		// redirect
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
+
 	tpl.ExecuteTemplate(w, "signup.gohtml", u)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	if alreadyLoggedIn(r) {
+	if alreadyLoggedIn(w, r) {
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
@@ -108,21 +102,18 @@ func login(w http.ResponseWriter, r *http.Request) {
 		un := r.FormValue("username")
 		p := r.FormValue("password")
 
-		// is there a username
-		u, ok := dbUsers[un]
+		u, ok := dbUser[un]
 		if !ok {
-			http.Error(w, "Username do not match", http.StatusForbidden)
+			http.Error(w, "Username does not match", http.StatusForbidden)
 			return
 		}
 
-		// does the entered password match the stored password?
 		err := bcrypt.CompareHashAndPassword(u.Password, []byte(p))
 		if err != nil {
-			http.Error(w, "Password do not match", http.StatusForbidden)
+			http.Error(w, "Password does not match", http.StatusForbidden)
 			return
 		}
 
-		// create session
 		sID, _ := uuid.NewV4()
 		c := &http.Cookie{
 			Name:  "session",
@@ -135,4 +126,27 @@ func login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	tpl.ExecuteTemplate(w, "login.gohtml", nil)
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	if !alreadyLoggedIn(w, r) {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	c, _ := r.Cookie("session")
+
+	// delete session
+	delete(dbSession, c.Value)
+
+	// remove the cookie
+	c = &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		MaxAge: -1,
+	}
+	http.SetCookie(w, c)
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+	return
 }
